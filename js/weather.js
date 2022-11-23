244c5c581dd31ce55992aabc6400bdc8bd09ea8d
heesoo <101074776+heesoo-enord@users.noreply.github.com>
2022-11-23 20:46:12 +0900

    파일명, ID 수정

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,15 @@
-var onion_month_high = document.getElementById("onion_month_high").getContext("2d");
-var onion_year_high = document.getElementById("onion_year_high").getContext("2d");
-var onion_month_low = document.getElementById("onion_month_low").getContext("2d");
-var onion_year_low = document.getElementById("onion_year_low").getContext("2d");
-var onion_month_high = new Chart(onion_month_high, {
+var temperatures = document.getElementById("temperatures").getContext("2d");
+var soil = document.getElementById("soil").getContext("2d");
+var windspeed = document.getElementById("windspeed").getContext("2d");
+var precipitation = document.getElementById("precipitation").getContext("2d");
+var temperatures = new Chart(temperatures, {
     type: "line",
     data: {
-        labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월"],
+        labels: ["2021-11-03", "2021-11-04", "2021-11-05", "2021-11-06", "2021-11-07", "2021-11-08", "2021-11-09"],
         datasets: [
             {
-                label: "2019",
-                data: [9228, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
+                label: "평균 기온",
+                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
@@ -29,8 +29,8 @@ var onion_month_high = new Chart(onion_month_high, {
                 borderWidth: 1,
             },
             {
-                label: "2020",
-                data: [13, 933, 19.6, 19.5, 22.2, 23.8, 18.3, 12],
+                label: "최고 기온",
+                data: [17.3, 19.6, 19.5, 22.2, 23.8, 18.3, 12],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
@@ -50,29 +50,8 @@ var onion_month_high = new Chart(onion_month_high, {
                 borderWidth: 1,
             },
             {
-                label: "2021",
-                data: [27, 389, 9.4, 8.6, 11.2, 9.4, 6.6, 6.9],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-            },
-            {
-                label: "2022",
-                data: [11, 568, 9.4, 8.6, 11.2, 9.4, 6.6, 6.9],
+                label: "최저 기온",
+                data: [6.9, 9.4, 8.6, 11.2, 9.4, 6.6, 6.9],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
@@ -102,13 +81,13 @@ var onion_month_high = new Chart(onion_month_high, {
     },
 });
 
-var onion_year_high = new Chart(onion_year_high, {
+var soil = new Chart(soil, {
     type: "line",
     data: {
-        labels: ["2017", "2018", "2019", "2020", "2021", "2022"],
+        labels: ["2021-11-03", "2021-11-04", "2021-11-05", "2021-11-06", "2021-11-07", "2021-11-08", "2021-11-09"],
         datasets: [
             {
-                label: "평균",
+                label: "평균 토양 수분",
                 data: [26.9, 26.6, 26.3, 26, 30.9, 32.5, 33.1],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
@@ -139,77 +118,14 @@ var onion_year_high = new Chart(onion_year_high, {
     },
 });
 
-var onion_month_low = new Chart(onion_month_low, {
+var windspeed = new Chart(windspeed, {
     type: "line",
     data: {
         labels: ["2021-11-03", "2021-11-04", "2021-11-05", "2021-11-06", "2021-11-07", "2021-11-08", "2021-11-09"],
         datasets: [
             {
-                label: "2019",
-                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-            },
-            {
-                label: "2020",
-                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-            },
-            {
-                label: "2021",
-                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
-                    "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-            },
-            {
-                label: "2022",
-                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
+                label: "평균 풍속",
+                data: [0.8, 1.2, 1.5, 1.6, 2.2, 1.7, 1.9],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
@@ -239,14 +155,56 @@ var onion_month_low = new Chart(onion_month_low, {
     },
 });
 
-var onion_year_low = new Chart(onion_year_low, {
+var precipitation = new Chart(precipitation, {
     type: "line",
     data: {
         labels: ["2021-11-03", "2021-11-04", "2021-11-05", "2021-11-06", "2021-11-07", "2021-11-08", "2021-11-09"],
         datasets: [
             {
-                label: "평균",
-                data: [26.9, 26.6, 26.3, 26, 30.9, 32.5, 33.1],
+                label: "일사량",
+                data: [11.6, 13.7, 14.7, 15.8, 16.9, 11.8, 9.2],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: "강수량",
+                data: [17.3, 19.6, 19.5, 22.2, 23.8, 18.3, 12],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: "일조 시간",
+                data: [6.9, 9.4, 8.6, 11.2, 9.4, 6.6, 6.9],
                 backgroundColor: [
                     "rgba(255, 99, 132, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
